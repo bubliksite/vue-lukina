9861d3a2a7244c8eab4611e2f88a1bb88132bff7
bubliksite <serj.solntsev@gmail.com>
2021-07-20 23:07:34 +0300

    Services Page

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,37 +1,76 @@
 <template>
   <div>
-    <Loader v-if="loader" />
-    <div v-else>
-      <Header />
-      <Slider v-if="homePage" :homePage="homePage" />
+    <Header />
+    <div v-if="page">
+      <carousel :items="1">
+        <div
+          class="slide position-relative"
+          v-for="(videoId, index) in page.CFS.video"
+          :key="index"
+        >
+          <Slide
+            :videoId="videoId"
+            :title="page.CFS.title[index]"
+            :subtitle="page.CFS.subtitle[index]"
+            :isShowButton="page.CFS.isShowButton[index] === '1'"
+          />
+        </div>
+      </carousel>
     </div>
+    <section class="bg-white" v-if="page">
+      <div class="container py-5">
+        <SectionHeader title="Направления" />
+        <p class="py-5 m-0">
+          В центре проводится психологическое консультирование в соответствии со
+          стандартами и требованиями ЕАС (Европейской Ассоциации
+          Психологического Консультирования) по всем основным направлениям:
+        </p>
+        <ul class="pb-3 list-unstyled">
+          <li
+            class="py-1"
+            v-for="(item, index) in page.CFS.direction"
+            :key="index"
+          >
+            <img :src="bullet" alt="" />
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="bg-primary" v-if="page">
+      <div class="container py-5">
+        <SectionHeader title="Стоимость консультаций" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-  import {actionTypes as pageActionTypes} from '../store/modules/getPage'
-  import Loader from '../components/Loader'
+  import axios from '../api/axios'
+  import carousel from 'vue-owl-carousel2'
+
   import Header from '../components/Header/Header'
-  import Slider from '../components/Home/Slider'
+  import SectionHeader from '../components/SectionHeader'
+  import Slide from '../components/Slide'
 
   export default {
     name: 'Services',
-    components: {Slider, Header, Loader},
+    components: {Header, SectionHeader, Slide, carousel},
     data() {
       return {
+        bullet: require('../assets/images/icons/icon-bullet.svg'),
         homePage: '',
-        loader: true
+        loader: true,
+        page: null
       }
     },
-    created() {
-      this.$store
-        .dispatch(pageActionTypes.getPage, {
-          pageId: 52
-        })
-        .then((response) => (this.homePage = response))
-    },
     mounted() {
-      this.loader = false
+      this.getPage()
+    },
+    methods: {
+      getPage() {
+        axios.get('/pages/52').then((response) => (this.page = response.data))
+      }
     }
   }
 </script>
